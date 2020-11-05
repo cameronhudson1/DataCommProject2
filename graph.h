c3d52638b1dcf4b317c5744325859d9ca130124b
cmrnhdudson@gmail.com <cmrnhudson@gmail.com>
2020-11-05 11:43:45 -0500

    Added Djikstra's code to graph library

--- graph.h
+++ graph.h
@@ -1,27 +1,23 @@
 #ifndef _GRAPH_H
 #define _GRAPH_H
 
-// A structure to represent an adjacency list node 
-struct AdjListNode 
-{ 
-    int dest; 
-    struct AdjListNode* next; 
-}; 
-  
-// A structure to represent an adjacency list 
-struct AdjList 
-{ 
-    struct AdjListNode *head;  
-}; 
-  
-// A structure to represent a graph. A graph 
-// is an array of adjacency lists. 
-// Size of array will be V (number of vertices  
-// in graph) 
-struct Graph 
-{ 
-    int V; 
-    struct AdjList* array; 
-}; 
+typedef struct 
+{
+	void *destination_node;
+	int weight;
+} Edge_t;
+
+typedef struct
+{
+	int num_edges;
+	Edge_t * edge_list;
+} GraphNode_t;
+
+typedef struct
+{
+	int num_nodes;
+	
+	GraphNode_t head;
+} Graph_t;
 
 #endif
